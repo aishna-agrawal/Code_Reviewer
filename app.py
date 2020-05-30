a6243bac938935c62b4840715db748bbbb478dcd
Priyam1418 <63399260+Priyam1418@users.noreply.github.com>
2020-05-30 12:00:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def index():
 
     return render_template("index.html")
     
-if __name__=='__app__':
-    app.run()
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port="2456") #change host and port accordingly.
