bf8841725b20b12dcb0e468d7ef8a9176d826893
Priyam1418 <63399260+Priyam1418@users.noreply.github.com>
2020-06-05 14:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,25 +7,27 @@ from flask import Flask, render_template,request
 
 app = Flask(__name__,template_folder='templates')
 
-@app.route('/',methods=["GET", "POST"])
+@app.route('/')
 def index():
-    if request.method == "POST":
-        req = request.form
-        lang = req.get("lang")
-        code = req.get("code")
-        features = main(code,lang)
-        CSV_filename = "<Filename>"  #Filename of the CSV file which should include outputs.
-        lst= list(features.values())
-        q = arr_input(lst)
-        lst.append(int(q[0]))
-        #features['result']=int(q[0])  #Uncomment for adding result in tht output.
-        result=features
-        with open(CSV_filename, 'a+', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(lst)
-        return render_template("reviews.html", lang =lang, code = code,res = result)
-
     return render_template("index.html")
+
+@app.route('/getCode',methods=["POST"])
+def getCode():
+    req = request.form
+    lang = req.get("lang")
+    code = req.get("code")
+    features = main(code,lang)
+    result = features
+    CSV_filename = "A.csv"  #Filename of the CSV file which should include outputs.
+    lst= list(features.values())
+    q = arr_input(lst)
+    lst.append(int(q[0]))
+    features['Result']=int(q[0])  #Uncomment for adding result in tht output.
+    result = features
+    with open(CSV_filename, 'a+', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(lst)
+    return render_template("reviews.html", lang =lang, code = code,res = result)
     
 if __name__=='__main__':
     app.run(host="0.0.0.0", port="2456") #change host and port accordingly.
